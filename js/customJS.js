1a1e64d65adeb5f5d42959e693a01626c3f3c35b
nagarjunbs <nagarjun323@gmail.com>
2015-12-05 22:41:56 +0530

    changed number

--- js/customJS.js
+++ js/customJS.js
@@ -7,7 +7,7 @@ myApp.directive("personalInfo",function(){
 	function linker (scope, element, attr) {
 
 		}
-	var data = '<div id="personalInfo" align="center"><h2>Nagarjun Sheshadri</h2><p><i>nagarjun323@gmail.com</i> &#8226 <b>8123285408</b><br>github.com/nagarjunbs</p></div>';
+	var data = '<div id="personalInfo" align="center"><h2>Nagarjun Sheshadri</h2><p><i>nagarjun323@gmail.com</i> &#8226 <b>xxxxxxx408</b><br>github.com/nagarjunbs</p></div>';
 
 		return{
 			restrict:'AE',
